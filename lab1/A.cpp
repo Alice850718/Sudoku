08622cef140d15043352f28673f0570537ada798
student <F74036221@2015cpp.(none)>
2015-03-11 23:06:00 +0800

    finish

--- lab1/A.cpp
+++ lab1/A.cpp
@@ -5,17 +5,12 @@ int main()
 	int n;
 	cin >> n;
 	cout << "" << n;
-	goto two;
 	two:
 	if( n%2 == 1 )
-	{
 		n = 3*n +1;
-	}
 	else
-	{
 		n = n/2;
-	}
-	cout << " " <<  n;
+		cout << " " <<  n;
 	if( n == 1)
 	{
 		cout << endl;
